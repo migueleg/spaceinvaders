f4187d5e4c9d1bf867985a5d1164be53f61f48e6
Miguel G <90210595+migueleg@users.noreply.github.com>
2023-06-03 14:12:08 -0500

    Add files via upload

---
+++ Space_Invaders_Miguel_Garcia/src/space_invaders_miguel_garcia/Panel.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package space_invaders_miguel_garcia;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Panel {
+
+}
---
+++ Space_Invaders_Miguel_Garcia/src/space_invaders_miguel_garcia/Space_Invaders_Miguel_Garcia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space_invaders_miguel_garcia;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Space_Invaders_Miguel_Garcia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
